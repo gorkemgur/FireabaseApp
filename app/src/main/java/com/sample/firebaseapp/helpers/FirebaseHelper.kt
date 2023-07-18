193a9eaa2a73e23522a28ecc968c109a6f8608ad
gorkemgur <86607113+gorkemgur@users.noreply.github.com>
2023-07-18 13:29:38 +0300

    Integrated Message Delete Flow && User Name Click Flow

--- app/src/main/java/com/sample/firebaseapp/helpers/FirebaseHelper.kt
+++ app/src/main/java/com/sample/firebaseapp/helpers/FirebaseHelper.kt
@@ -5,14 +5,17 @@ import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.ktx.database
+import com.google.firebase.database.ktx.getValue
 import com.google.firebase.ktx.Firebase
 import com.sample.firebaseapp.model.UserModel
 
 object FirebaseHelper {
 
+    private val reference = Firebase.database.reference
+
     fun getCurrentUserModel(callback: (UserModel?) -> Unit) {
         if (Firebase.auth.currentUser != null) {
-            Firebase.database.reference.child("Users").child(Firebase.auth.currentUser?.uid ?: "")
+            reference.child("Users").child(Firebase.auth.currentUser?.uid ?: "")
                 .addValueEventListener(object : ValueEventListener {
                     override fun onDataChange(snapshot: DataSnapshot) {
                         callback(snapshot.getValue(UserModel::class.java))
@@ -25,5 +28,18 @@ object FirebaseHelper {
         } else {
             callback(null)
         }
+    }
+
+
+    fun getUserModelWithUserId(userId: String?, callback: (UserModel?) -> Unit) {
+        reference.child("Users").child(userId ?: "").addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                callback(snapshot.getValue(UserModel::class.java))
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                callback(null)
+            }
+        })
     }
 }
